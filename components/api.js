c6026b327e1ee9abc9e6ca419c44533e1f349bf5
wealthoceaninstitute-commits <wealthoceaninstitute@gmail.com>
2025-11-27 15:53:23 +0530

    Update api.js

--- components/api.js
+++ components/api.js
@@ -1,4 +1,3 @@
-// components/api.js
 import axios from 'axios';
 import API_BASE from '../src/lib/apiBase.js';
 
@@ -6,4 +5,19 @@ const api = axios.create({
   baseURL: API_BASE,
   headers: { 'Content-Type': 'application/json' },
 });
+
+// 🔥 Automatically inject user token
+api.interceptors.request.use(
+  (config) => {
+    if (typeof window !== "undefined") {
+      const token = localStorage.getItem("authToken"); // or woi_token
+      if (token) {
+        config.headers["X-Auth-Token"] = token;
+      }
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default api;
