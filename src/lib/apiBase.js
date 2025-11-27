b00422bfd6b0e064add416f73fa723af28a8e92d
wealthoceaninstitute-commits <wealthoceaninstitute@gmail.com>
2025-11-27 15:09:48 +0530

    Update apiBase.js

--- src/lib/apiBase.js
+++ src/lib/apiBase.js
@@ -1,3 +1,4 @@
-// src/lib/apiBase.js
-const API_BASE = (process.env.NEXT_PUBLIC_API_BASE || '').replace(/\/$/, '');
-export default API_BASE;
+headers: {
+  "Content-Type": "application/json",
+  "x-auth-token": localStorage.getItem("woi_token") || "",
+}
