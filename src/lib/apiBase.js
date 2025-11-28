8f65928ce609fd5cd6fbbe7f542338caa86342e6
wealthoceaninstitute-commits <wealthoceaninstitute@gmail.com>
2025-11-28 13:41:51 +0530

    Delete src/lib directory

--- src/lib/apiBase.js
+++
@@ -1,9 +0,0 @@
-// src/lib/apiBase.js
-
-// Base URL for your FastAPI backend.
-// Railway will inject NEXT_PUBLIC_API_BASE from your env.
-const API_BASE =
-  process.env.NEXT_PUBLIC_API_BASE || "http://localhost:8000";
-
-// Components (like components/api.js) import this.
-export default API_BASE;
