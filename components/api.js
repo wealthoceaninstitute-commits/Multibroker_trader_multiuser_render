c428c161d7edaaa2b81dd674f974a8a795eae015
wealthoceaninstitute-commits <wealthoceaninstitute@gmail.com>
2025-12-19 13:46:00 +0530

    Delete components directory

--- components/api.js
+++
@@ -1,9 +0,0 @@
-// components/api.js
-import axios from 'axios';
-import API_BASE from '../src/lib/apiBase.js';
-
-const api = axios.create({
-  baseURL: API_BASE,
-  headers: { 'Content-Type': 'application/json' },
-});
-export default api;
