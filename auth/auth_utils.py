c8474edb3597b753b58ab450fc44827393d75127
wealthoceaninstitute-commits <wealthoceaninstitute@gmail.com>
2026-02-08 22:58:35 +0530

    Delete auth directory

--- auth/auth_utils.py
+++
@@ -1,7 +0,0 @@
-# auth/auth_utils.py
-from passlib.context import CryptContext
-
-_pwd = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-def hash_password(password: str) -> str:
-    return _pwd.hash(password)
